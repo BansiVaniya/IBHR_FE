c7f0f70365fc0b88a0ffdba5a2c7de1b2c39ecd4
pratiksha2609 <pratiksha.g@techroversolutions.com>
2024-02-02 17:38:57 +0530

    [PG] forgot password component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { LoginComponent } from './shared/components/auth/login/login.component';
+import { ForgotPasswordComponent } from './shared/components/auth/forgot-password/forgot-password/forgot-password.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LoginComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    LoginComponent, 
+    ForgotPasswordComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './shared/components/auth/login/login.component';
+import { ForgotPasswordComponent } from './shared/components/auth/forgot-password/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
+    {path: 'app-forgot-password', component: ForgotPasswordComponent}
 ];
