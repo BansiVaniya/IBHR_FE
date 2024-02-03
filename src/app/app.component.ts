1a4d56560114e4f0982efc5144203e1269430f21
DELL <nilesh.b@techroversolutions.com>
2024-02-03 12:39:03 +0530

    [BV}email validation helper class create,form module add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { LoginComponent } from './shared/components/auth/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+// import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/auth/login/login.component.ts
+++ src/app/shared/components/auth/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {EMAIL_PATTERN} from '../../../helpers/emailValidation';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+// import { MatNativeDateModule } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm : FormGroup;
+  loginSubmitted:boolean=false;
+constructor(){
+  this.loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(EMAIL_PATTERN),
+    ]),
+    password:new FormControl('',[Validators.required])
+  });
+}
+ngOnInit() {
+
+}
+login(){
+
+}
 
 }
